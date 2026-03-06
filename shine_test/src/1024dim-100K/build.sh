d18c56ecffcb04f6e44a39510ed581ec5235d3f7
diandianjunA <1730523754@qq.com>
2026-03-06 16:14:16 +0800

    修改索引加载地址

--- shine_test/src/1024dim-100K/build.sh
+++ shine_test/src/1024dim-100K/build.sh
@@ -2,5 +2,6 @@ python3 test_vector_test.py \
     --option build \
     --host localhost \
     --port 8080 \
-    --data_path /path/to/train.fbin \
+    --data_path /data/xjs/random_dataset/1024dim100K/base.fbin \
+    --index_path /data/xjs/index/shine_index/1024dim100K \
     --dim 1024
